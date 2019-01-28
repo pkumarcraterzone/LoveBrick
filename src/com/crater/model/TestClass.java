b5ea9bd33062c5f69a6699f818eadf151b50e746
pkumarcraterzone <nsbisht@craterzone.com>
2019-01-28 16:32:09 +0530

    testclass edited

--- src/com/crater/model/TestClass.java
+++ src/com/crater/model/TestClass.java
@@ -107,6 +107,19 @@ public class TestClass {
 		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
 		
 		
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		brick1.addFlag(new Flag(user1, "hhhhhhhhhhh"));
+		
+		
 		
 		//flags usecase
 		System.out.println(brick1.isVisible());
